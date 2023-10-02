7578bdb179c737ea8b53858c6279ff433a535fb0
Rafał Masiarek <rafal@masiarek.pl>
2023-10-03 01:12:25 +0200

    feat: add pre-commit installing via pip3

--- Dockerfile.bash
+++ Dockerfile.bash
@@ -83,7 +83,7 @@ SHELL ["/usr/local/bin/bash", "-c"]
 RUN set -eux \
     && apk --no-cache update \
     && apk --no-cache add python3 py-pip py-setuptools ca-certificates groff less bash git jq file curl gomplate openssh-client \
-    && pip --no-cache-dir install awscli terraform-local \
+    && pip --no-cache-dir install awscli terraform-local pre-commit \
     && echo -e '/usr/bin/curl -s -L https://raw.githubusercontent.com/Infrastrukturait/READMEgen/main/README.md.template |\\\n\t/usr/bin/gomplate -d config=./README.json > ./README.md' > /usr/local/bin/readmegen \
     && echo -e '/usr/bin/aws ${AWS_ENDPOINT_OVERRIDE:+--endpoint-url $AWS_ENDPOINT_OVERRIDE} "$@"' > /usr/local/bin/aws \
     && chmod +x /usr/local/bin/readmegen /usr/local/bin/aws \
